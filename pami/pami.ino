8cdd298d1615dd1c9e87aa90de691ab05388dfae
arduilol <equipe@electrobot.fr>
2024-12-17 22:04:20 +0100

    Session du 15/01

--- pami/pami.ino
+++ pami/pami.ino
@@ -1,31 +1,32 @@
 #include <AccelStepper.h>
- 
-// Define some steppers and the pins the will use
+
 AccelStepper stepper2(1, 2, 5);
 AccelStepper stepper3(1, 3, 6);
- 
-void setup()
-{  
-    pinMode(8, OUTPUT);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Init");
+
+  pinMode(8, OUTPUT);
   digitalWrite(8, LOW);
 
-    stepper2.setMaxSpeed(2000.0);
-    stepper2.setAcceleration(2000.0);
-    stepper2.moveTo(10000);
-    
-    stepper3.setMaxSpeed(2000.0);
-    stepper3.setAcceleration(2000.0);
-    stepper3.moveTo(-10000); 
-}
- 
-void loop()
-{
-    // Change direction at the limits
-    if (stepper2.distanceToGo() == 0) {
-      //   stepper2.moveTo(-stepper2.currentPosition());
-      //  stepper3.moveTo(-stepper3.currentPosition());
-    }
+  stepper2.setMaxSpeed(2000.0);
+  stepper2.setAcceleration(2000.0);
+  stepper2.moveTo(1000);
 
-    stepper2.run();
-    stepper3.run();
+  stepper3.setMaxSpeed(2000.0);
+  stepper3.setAcceleration(2000.0);
+  stepper3.moveTo(1000);
+}
+
+void loop() {
+  // Change direction at the limits
+  if (stepper3.distanceToGo() == 0) {
+    Serial.println("Changement de sens...");
+    stepper2.moveTo(-stepper2.currentPosition());
+    stepper3.moveTo(-stepper3.currentPosition());
+  }
+
+  stepper2.run();
+  stepper3.run();
 }
