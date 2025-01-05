646a71997c08be20e0011151cbb6fa50e5e708ec
arduilol <equipe@electrobot.fr>
2025-01-05 20:56:09 +0100

    2 ascenceurs

--- ascenceur/src/main.ino
+++ ascenceur/src/main.ino
@@ -1,17 +1,24 @@
 #include <Servo.h>
+#include <Arduino.h>
 
 Servo servoPince;
 Servo servoAcs;
+Servo servoPince2;
+Servo servoAcs2;
 
 void setup() {
   Serial.begin(9600); 
   servoPince.attach(9);  
-  servoAcs.attach(10);  
+  servoAcs.attach(10);
+  servoPince2.attach(8);  
+  servoAcs2.attach(11);  
   
   Serial.println("Servo Control Initialized");
 
   servoAcs.write(90);
   servoPince.write(45);
+  servoAcs2.write(90);
+  servoPince2.write(45);
 }
 
 void loop() {
@@ -19,16 +26,20 @@ void loop() {
     int cmd = Serial.parseInt();
 
     if (cmd == 1) {
-      servoAcs.write(20);
+      servoAcs.write(60);
+      servoAcs2.write(60);
     }
     else if (cmd == 2) {
       servoAcs.write(120);
+      servoAcs2.write(120);
     }
     else if (cmd == 3) {
       servoPince.write(1);
+      servoPince2.write(1);
     }
     else if (cmd == 4) {
       servoPince.write(90);
+      servoPince2.write(90);
     }
     
     // Ensure the angle is within the valid range (0-180 degrees)
